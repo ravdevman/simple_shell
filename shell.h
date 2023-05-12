9ca0af607e7a9d5ca562dcbd0f193432066cd697
seroking <baridmohamedamine004@gmail.com>
2023-05-12 14:35:31 -0700

    header

---
+++ shell.h
@@ -0,0 +1,20 @@
+								/* by Rayane Madoune && Mohamed Amine Barid */
+#ifndef SHELL_H
+#define SHELL_H
+
+										/*Libraries*/
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
+#include <ctype.h>
+#include <errno.h>
+
+#endif
